23581f25f7ea29159e1eb3a6c0c663228c13beef
kbventures <kmb_x@hotmail.com>
2022-11-02 18:31:19 -0600

    Back End Handles CORS

--- app.js
+++ app.js
@@ -10,6 +10,21 @@ const dbConnect = require("./db/dbConnect");
 
 // execute database connection
 dbConnect();
+
+// Curb Cores Error by adding a header here
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 const User = require("./db/userModel");
 
 // body parser configuration
