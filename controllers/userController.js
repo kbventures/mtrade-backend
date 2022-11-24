f87dd796a8e688c83e02aade7a31f416ae650e01
kbventures <kmb_x@hotmail.com>
2022-11-24 12:21:00 -0600

    signing up and hashing passwords

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,16 @@ const loginUser = async (req, res) => {
 
 //signup user
 const signupUser = async (req, res) => {
-  res.json({ mssg: "signup user" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+  _;
 };
 
 module.exports = { signupUser, loginUser };
